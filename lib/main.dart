a28f069a64abcfc9501ec267852f2fa1102cca3e
rajamransri-blip <rajamransri@gmail.com>
2026-03-08 09:17:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Toggle File Tool',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const HomePage(),
+      title: 'Toggle Tool',
+
+      theme: ThemeData(
+        cardTheme: const CardThemeData(
+          elevation: 3,
+        ),
+      ),
+
+      home: HomePage(),
     );
   }
 }
