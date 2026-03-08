cc4037978fada301b9524e84aca2786ed102389a
rajamransri-blip <rajamransri@gmail.com>
2026-03-08 09:03:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final ThemeData baseTheme = ThemeData.dark();
+
     return MaterialApp(
       title: 'GFX Raaz',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
+
+      theme: baseTheme.copyWith(
+        useMaterial3: true,
+
         scaffoldBackgroundColor: const Color(0xFF0A0A0F),
+
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFF1A1A2E),
           elevation: 0,
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w600),
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
         ),
-        cardColor: const Color(0xFF1E1E2F),
+
+        cardTheme: const CardTheme(
+          color: Color(0xFF1E1E2F),
+          elevation: 4,
+          margin: EdgeInsets.all(12),
+        ),
       ),
+
       home: const HomePage(),
     );
   }
