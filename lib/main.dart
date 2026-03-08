af1cf112b047953851e7ff1d6bb911c7cc1524f3
rajamransri-blip <rajamransri@gmail.com>
2026-03-08 09:10:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'home_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,36 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final ThemeData baseTheme = ThemeData.dark();
-
     return MaterialApp(
-      title: 'GFX Raaz',
+      title: 'Toggle File Tool',
       debugShowCheckedModeBanner: false,
-
-      theme: baseTheme.copyWith(
-        useMaterial3: true,
-
-        scaffoldBackgroundColor: const Color(0xFF0A0A0F),
-
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF1A1A2E),
-          elevation: 0,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-
-        cardTheme: const CardTheme(
-          color: Color(0xFF1E1E2F),
-          elevation: 4,
-          margin: EdgeInsets.all(12),
-        ),
-      ),
-
+      theme: ThemeData.dark(),
       home: const HomePage(),
     );
   }
